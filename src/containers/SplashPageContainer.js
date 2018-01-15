925ba085fac81b6893cdb2e9141d52717cc1d7cd
Gonzodamus <Dick@TheDickWard.com>
2018-01-15 11:16:55 -0500

    added back button to login/signup

--- src/containers/SplashPageContainer.js
+++ src/containers/SplashPageContainer.js
@@ -8,7 +8,7 @@ class SplashPageContainer extends React.Component {
     active: null
   };
   handleClick = event => {
-    if (this.state.active === event.target.value) {
+    if (this.state.active != null) {
       this.setState({ active: null });
     } else {
       this.setState({ active: event.target.value });
@@ -17,12 +17,22 @@ class SplashPageContainer extends React.Component {
   render() {
     return (
       <div>
-        <Button onClick={this.handleClick} className="Login" value="Login">
-          Log In
-        </Button>
-        <Button onClick={this.handleClick} className="Signup" value="Signup">
-          Sign Up
-        </Button>
+        {this.state.active === null ? (
+          <div>
+            <Button onClick={this.handleClick} className="Login" value="Login">
+              Log In
+            </Button>
+            <Button
+              onClick={this.handleClick}
+              className="Signup"
+              value="Signup"
+            >
+              Sign Up
+            </Button>
+          </div>
+        ) : (
+          <Button onClick={this.handleClick}>Back</Button>
+        )}
         {this.state.active === "Login" ? <Login /> : null}
         {this.state.active === "Signup" ? <Signup /> : null}
       </div>
